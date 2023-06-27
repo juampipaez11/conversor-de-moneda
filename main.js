864a12a8ee2b7e8bdde9d4e160c799da29f42b0b
juampiPaez11 <juampipaez11@gmail.com>
2023-06-27 18:28:34 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function convertir() {
+    var pesos = parseFloat(document.getElementById('pesos').value);
+    var tasaCambio = 0.0039; 
+    var dolares = pesos * tasaCambio;
+    console.log(pesos + ' pesos argentinos son ' + dolares + ' dólares estadounidenses.');
+  }
+  
