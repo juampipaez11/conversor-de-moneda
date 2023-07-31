f0a12c68ed7ccc960de133efb0a270415b745a1d
juampiPaez11 <juampipaez11@gmail.com>
2023-07-31 18:53:52 -0300

    tercera entrega

--- main.js
+++ main.js
@@ -1,31 +1,52 @@
-function capturarEntradas() {
-  let pesos;
-  do {
-    const inputPesos = prompt('Ingrese la cantidad de pesos argentinos:');
-    pesos = parseFloat(inputPesos);
-  } while (isNaN(pesos) || !Number.isFinite(pesos));
+let resultadoActual = 0;
 
-  const opcionesMoneda = ['Dólar', 'Euro', 'Libra'];
-  let monedaDestino;
-  do {
-    const seleccion = prompt('Seleccione la moneda a la que desea convertir:\n1. Dólar\n2. Euro\n3. Libra');
-    switch (seleccion) {
-      case '1':
-        monedaDestino = opcionesMoneda[0];
-        break;
-      case '2':
-        monedaDestino = opcionesMoneda[1];
-        break;
-      case '3':
-        monedaDestino = opcionesMoneda[2];
-        break;
-      default:
-        monedaDestino = '';
-        break;
-    }
-  } while (monedaDestino === '');
+function obtenerPesos() {
+  const inputPesos = document.getElementById('inputPesos').value;
+  const pesos = parseFloat(inputPesos);
 
-  return { pesos, monedaDestino };
+  if (isNaN(pesos) || !Number.isFinite(pesos)) {
+    mostrarMensaje('Debe ingresar una cantidad de pesos válida.', 'error');
+    return null;
+  }
+
+  return pesos;
+}
+
+function obtenerMonedaDestino() {
+  const selectMoneda = document.getElementById('selectMoneda');
+  const monedaDestino = selectMoneda.value;
+
+  if (monedaDestino === '') {
+    mostrarMensaje('Debe seleccionar una moneda de destino.', 'error');
+    return null;
+  }
+
+  return monedaDestino;
+}
+
+function mostrarMensaje(mensaje, tipo) {
+  const resultadoHTML = document.getElementById('resultado');
+  resultadoHTML.textContent = mensaje;
+
+  if (tipo === 'error') {
+    resultadoHTML.style.color = 'red';
+  } else {
+    resultadoHTML.style.color = 'black';
+  }
+}
+
+function mostrarResultadoEnHTML(resultado, monedaDestino) {
+  const resultadoHTML = document.getElementById('resultado');
+  resultadoHTML.textContent = 'El resultado de la conversión es: ' + resultado + ' ' + monedaDestino;
+  resultadoHTML.style.color = 'black';
+  resultadoActual = resultado;
+}
+
+function agregarAlHistorial(accion, cantidad, monedaOrigen, monedaDestino, resultado) {
+  const historialHTML = document.getElementById('historial');
+  const nuevoHistorial = document.createElement('div');
+  nuevoHistorial.textContent = accion + ' ' + cantidad + ' ' + monedaOrigen + ' a ' + monedaDestino + ': ' + resultado;
+  historialHTML.appendChild(nuevoHistorial);
 }
 
 function obtenerTasaDeCambio(monedaDestino) {
@@ -51,86 +72,88 @@ function realizarOperacion(pesos, monedaDestino) {
   return resultado;
 }
 
-function mostrarResultado(resultado, monedaDestino) {
-  alert('El resultado de la conversión es: ' + resultado + ' ' + monedaDestino);
-}
-
-
-function operarResultado(resultado, monedaDestino) {
-  const opcionesOperacion = ['Sumar', 'Restar', 'Multiplicar', 'Dividir'];
-  let operacion;
-  do {
-    const seleccion = prompt('Seleccione la operación a realizar:\n1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir');
-    switch (seleccion) {
-      case '1':
-        operacion = opcionesOperacion[0];
-        break;
-      case '2':
-        operacion = opcionesOperacion[1];
-        break;
-      case '3':
-        operacion = opcionesOperacion[2];
-        break;
-      case '4':
-        operacion = opcionesOperacion[3];
-        break;
-      default:
-        operacion = '';
-        break;
-    }
-  } while (operacion === '');
-
-  if (!operacion) {
-    return resultado;
-  }
-
-
-  let nuevoResultado;
-  if (operacion === 'Sumar') {
-    const opcionSuma = prompt('Seleccione la opción a sumar:\n1. Pesos Argentinos\n2. ' + monedaDestino);
-    if (opcionSuma === '1') {
-      const sumaPesos = parseFloat(prompt('Ingrese la cantidad de pesos argentinos a sumar:'));
-      const sumaPesosConvertidos = realizarOperacion(sumaPesos, monedaDestino);
-      nuevoResultado = resultado + sumaPesosConvertidos;
-    } else if (opcionSuma === '2') {
-      const sumaMoneda = parseFloat(prompt('Ingrese la cantidad de ' + monedaDestino + ' a sumar:'));
-      nuevoResultado = resultado + sumaMoneda;
-    }
-  } else if (operacion === 'Restar') {
-    const opcionResta = prompt('Seleccione la opción a restar:\n1. Pesos Argentinos\n2. ' + monedaDestino);
-    if (opcionResta === '1') {
-      const restaPesos = parseFloat(prompt('Ingrese la cantidad de pesos argentinos a restar:'));
-      const restaPesosConvertidos = realizarOperacion(restaPesos, monedaDestino);
-      nuevoResultado = resultado - restaPesosConvertidos;
-    } else if (opcionResta === '2') {
-      const restaMoneda = parseFloat(prompt('Ingrese la cantidad de ' + monedaDestino + ' a restar:'));
-      nuevoResultado = resultado - restaMoneda;
-    }
-  } else if (operacion === 'Multiplicar') {
-    const multiplicacion = parseFloat(prompt('Ingrese el factor de multiplicación:'));
-    nuevoResultado = resultado * multiplicacion;
-  } else if (operacion === 'Dividir') {
-    const division = parseFloat(prompt('Ingrese el divisor:'));
-    nuevoResultado = resultado / division;
-  }
-
-  return nuevoResultado;
-}
-
-
 function convertir() {
-  const { pesos, monedaDestino } = capturarEntradas();
-  const resultado = realizarOperacion(pesos, monedaDestino);
-  mostrarResultado(resultado, monedaDestino);
+  const pesos = obtenerPesos();
+  const monedaDestino = obtenerMonedaDestino();
 
-  const nuevoResultado = operarResultado(resultado, monedaDestino);
-  mostrarResultado(nuevoResultado, monedaDestino);
+  if (pesos !== null && monedaDestino !== null) {
+    const resultado = realizarOperacion(pesos, monedaDestino);
+    mostrarResultadoEnHTML(resultado, monedaDestino);
+    agregarAlHistorial('Conversión', pesos, 'Pesos Argentinos', monedaDestino, resultado);
+  }
 }
 
-window.addEventListener('load', function() {
-  const btnConvertir = document.getElementById('btnConvertir');
-  btnConvertir.addEventListener('click', convertir); 
+function sumarPesos(pesos) {
+  resultadoActual += pesos;
+  return resultadoActual;
+}
+
+function restarPesos(pesos) {
+  resultadoActual -= pesos;
+  return resultadoActual;
+}
+
+document.getElementById('btnConvertir').addEventListener('click', convertir);
+
+document.getElementById('btnSumar').addEventListener('click', function() {
+  const pesos = parseFloat(document.getElementById('inputSumarRestar').value);
+  if (isNaN(pesos)) {
+    mostrarMensaje('Debe ingresar una cantidad de pesos válida.', 'error');
+    return;
+  }
+  const nuevoResultado = sumarPesos(pesos);
+  mostrarResultadoEnHTML(nuevoResultado, 'Pesos Argentinos');
+  agregarAlHistorial('Suma', pesos, 'Pesos Argentinos', 'Pesos Argentinos', nuevoResultado);
 });
+
+document.getElementById('btnRestar').addEventListener('click', function() {
+  const pesos = parseFloat(document.getElementById('inputSumarRestar').value);
+  if (isNaN(pesos)) {
+    mostrarMensaje('Debe ingresar una cantidad de pesos válida.', 'error');
+    return;
+  }
+  const nuevoResultado = restarPesos(pesos);
+  mostrarResultadoEnHTML(nuevoResultado, 'Pesos Argentinos');
+  agregarAlHistorial('Resta', pesos, 'Pesos Argentinos', 'Pesos Argentinos', nuevoResultado);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
